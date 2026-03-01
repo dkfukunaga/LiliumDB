9e6bacefe33d62a7f6a5f05659b9866bd4fa1cdf
dkfukunaga <dkfukunaga@gmail.com>
2026-03-01 13:57:31 -0800

    refactored Status to use factory constructors, made Status::Code an enum class, added isOk() and is(Code c).

--- src/common/status.h
+++ src/common/status.h
@@ -2,12 +2,14 @@
 #define LILIUMDB_STATUS_H
 
 #include <string>
+#include <string_view>
+#include <utility>
 
 namespace LiliumDB {
 
 class Status {
 public:
-    enum Code {
+    enum class Code {
         Ok,
         NewFile,
         ErrFile,
@@ -16,14 +18,22 @@ public:
         Error
     };
 
-    Status(Code c, std::string msg = ""): code_(c), message_(std::move(msg)) {}
+    static Status       ok() noexcept { return Status(Code::Ok); }
+    static Status       newFile(std::string_view msg) { return Status(Code::NewFile, std::string(msg)); }
+    static Status       file(std::string_view msg) { return Status(Code::ErrFile, std::string(msg)); }
+    static Status       io(std::string_view msg) { return Status(Code::ErrIO, std::string(msg)); }
+    static Status       corrupt(std::string_view msg) { return Status(Code::ErrCorrupt, std::string(msg)); }
+    static Status       error(std::string_view msg) { return Status(Code::Error, std::string(msg)); }
 
-    explicit operator bool() const { return code_ == Ok; }
+    Code                code() const noexcept { return code_; }
+    const std::string&  message() const { return message_; }
 
-    Code code() const { return code_; }
-    const std::string& message() const { return message_; }
-
+    bool                isOk() const noexcept { return code_ == Code::Ok; }
+    bool                is(Code c) const noexcept { return code_ == c; }
 private:
+    explicit Status(Code c) noexcept: code_(c) {}
+    Status(Code c, std::string msg): code_(c), message_(std::move(msg)) {}
+
     Code code_;
     std::string message_;
 };
