c90214b285c5b4c2fe8d8075df61f6331a140222
dkfukunaga <dkfukunaga@gmail.com>
2026-03-02 15:30:53 -0800

    created tests for ByteSpan, ByteView, and Status.

---
+++ tests/byte_span_test.cpp
@@ -0,0 +1,117 @@
+#include "common/byte_span.h"
+
+#include <vector>
+
+#include <gtest/gtest.h>
+
+using namespace LiliumDB;
+
+TEST(ByteSpanTest, Basic) {
+    std::vector<uint8_t> data = {1, 2, 3, 4, 5};
+    ByteSpan span(data.data(), data.size());
+
+    EXPECT_EQ(span.size(), 5);
+    EXPECT_EQ(span[0], 1);
+    EXPECT_EQ(span[4], 5);
+
+    span[2] = 42;
+    EXPECT_EQ(span[2], 42);
+
+    auto subspan = span.subspan(1, 3);
+    EXPECT_EQ(subspan.size(), 3);
+    EXPECT_EQ(subspan[0], 2);
+    EXPECT_EQ(subspan[2], 4);
+
+    auto subview = span.subview(0, 4);
+    EXPECT_EQ(subview.size(), 4);
+    EXPECT_EQ(subview[0], 1);
+    EXPECT_EQ(subview[3], 4);
+}
+
+TEST(ByteSpanTest, SubspanOutOfRange) {
+    std::vector<uint8_t> data = {1, 2, 3};
+    ByteSpan span(data.data(), data.size());
+
+    EXPECT_THROW(span.subspan(2, 2), std::out_of_range);
+    EXPECT_THROW(span.subspan(3, 1), std::out_of_range);
+}
+
+TEST(ByteSpanTest, WriteAndCopy) {
+    std::vector<uint8_t> data = {0, 0, 0, 0, 0};
+    ByteSpan span(data.data(), data.size());
+
+    uint8_t src[] = {10, 20, 30};
+    span.write(1, src, 3);
+    EXPECT_EQ(span[1], 10);
+    EXPECT_EQ(span[2], 20);
+    EXPECT_EQ(span[3], 30);
+    EXPECT_EQ(span[4], 0);
+
+    span.copy_within(0, 2, 3);
+    EXPECT_EQ(span[0], 20);
+    EXPECT_EQ(span[1], 30);
+    EXPECT_EQ(span[2], 0);
+    EXPECT_EQ(span[3], 30);
+    EXPECT_EQ(span[4], 0);
+}
+
+TEST(ByteSpanTest, OutOfRange) {
+    std::vector<uint8_t> data = {1, 2, 3};
+    ByteSpan span(data.data(), data.size());
+
+    EXPECT_THROW(span.subspan(2, 2), std::out_of_range);
+    EXPECT_THROW(span.subview(3, 1), std::out_of_range);
+}
+
+TEST(ByteSpanTest, Iterators) {
+    std::vector<uint8_t> data = {10, 20, 30};
+    ByteSpan span(data.data(), data.size());
+
+    std::vector<uint8_t> collected(span.begin(), span.end());
+    EXPECT_EQ(collected, data);
+
+    std::vector<uint8_t> reverse_collected(span.rbegin(), span.rend());
+    EXPECT_EQ(reverse_collected, std::vector<uint8_t>({30, 20, 10}));
+}
+
+TEST(ByteViewTest, Basic) {
+    std::vector<uint8_t> data = {5, 10, 15, 20};
+    ByteSpan span(data.data(), data.size());
+    ByteView view(span);
+
+    EXPECT_EQ(view.size(), 4);
+    EXPECT_EQ(view[0], 5);
+    EXPECT_EQ(view[3], 20);
+
+    auto subview = view.subview(1, 2);
+    EXPECT_EQ(subview.size(), 2);
+    EXPECT_EQ(subview[0], 10);
+    EXPECT_EQ(subview[1], 15);
+}
+
+TEST(ByteViewTest, SubviewOutOfRange) {
+    std::vector<uint8_t> data = {1, 2, 3};
+    ByteView view(data.data(), data.size());
+
+    EXPECT_THROW(view.subview(2, 2), std::out_of_range);
+    EXPECT_THROW(view.subview(3, 1), std::out_of_range);
+}
+
+TEST(ByteViewTest, OutOfRange) {
+    std::vector<uint8_t> data = {1, 2, 3};
+    ByteView view(data.data(), data.size());
+
+    EXPECT_THROW(view.subview(2, 2), std::out_of_range);
+    EXPECT_THROW(view.subview(3, 1), std::out_of_range);
+}
+
+TEST(ByteViewTest, Iterators) {
+    std::vector<uint8_t> data = {100, 200, 255};
+    ByteView view(data.data(), data.size());
+
+    std::vector<uint8_t> collected(view.begin(), view.end());
+    EXPECT_EQ(collected, data);
+
+    std::vector<uint8_t> reverse_collected(view.rbegin(), view.rend());
+    EXPECT_EQ(reverse_collected, std::vector<uint8_t>({255, 200, 100}));
+}
