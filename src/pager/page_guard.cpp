c0395d53fa93fe82098bf6c28f2bb79f78f9a262
dkfukunaga <dkfukunaga@gmail.com>
2026-03-03 23:53:01 -0800

    moved PageGuard to its own file.
    - made constructor public; trying to hide it was just needless complication. should make note that only Pager should create a PageGuard
    - constructor pins (increments pin count) and destructor unpins
    - finalized interface: span() returns mutable ByteSpan and marks dirty; view() returns immutable ByteView; pageNum() returns pageNum

---
+++ src/pager/page_guard.cpp
@@ -0,0 +1,63 @@
+#include "page_guard.h"
+
+#include "pager.h"
+#include "common/types.h"
+
+
+namespace LiliumDB {
+
+PageGuard::PageGuard(Pager* pager, PageNum pageNum, ByteSpan data)
+    : pager_(pager)
+    , pageNum_(pageNum)
+    , data_(data){
+    pager_->pinPage(pageNum);
+}
+
+PageGuard::PageGuard(PageGuard&& other) noexcept
+    : pager_(other.pager_)
+    , pageNum_(other.pageNum_)
+    , data_(other.data_) {
+    // invalidate other PageGuard
+    other.pager_ = nullptr;
+    other.pageNum_  = INVALID_PAGE;
+    other.data_     = ByteSpan(nullptr, 0);
+}
+
+PageGuard::~PageGuard() {
+    if (pager_) {
+        pager_->unpinPage(pageNum());
+    }
+}
+
+ByteSpan PageGuard::span() {
+    if (pager_) { 
+        pager_->markDirty(pageNum());
+    }
+    return data_;
+}
+
+ByteView PageGuard::view() const {
+    return ByteView(data_);
+}
+
+PageGuard& PageGuard::operator=(PageGuard&& other) noexcept {
+    if (this != &other) {
+        // release current page
+        if (pager_) {
+            pager_->unpinPage(pageNum_);
+        }
+
+        // move ownership from other PageGuard
+        pager_      = other.pager_;
+        pageNum_    = other.pageNum_;
+        data_       = other.data_;
+
+        // invalidate other PageGuard
+        other.pager_    = nullptr;
+        other.pageNum_  = INVALID_PAGE;
+        other.data_     = ByteSpan(nullptr, 0);
+    }
+    return *this;
+}
+
+} // namespace LiliumDB
---
+++ src/pager/page_guard.h
@@ -0,0 +1,35 @@
+#ifndef LILIUMDB_PAGE_GUARD_H
+#define LILIUMDB_PAGE_GUARD_H
+
+#include "common/types.h"
+#include "common/byte_span.h"
+
+namespace LiliumDB {
+
+class Pager;
+
+class PageGuard {
+public:
+    PageGuard(Pager* pager, PageNum pageNum, ByteSpan data);
+    PageGuard(const PageGuard&) = delete;
+    PageGuard(PageGuard&& other) noexcept;
+    ~PageGuard();
+
+    // automatically mark dirty on return of a mutable span
+    ByteSpan        span();
+    // do not mark dirty on return of immutable view
+    ByteView        view() const;
+    PageNum         pageNum() const { return pageNum_; }
+
+    // no copy assignment; move only
+    PageGuard& operator=(const PageGuard&) = delete;
+    PageGuard& operator=(PageGuard&& other) noexcept;
+private:
+    Pager*      pager_;
+    PageNum     pageNum_;
+    ByteSpan    data_;
+};
+
+} // namespace LiliumDB
+
+#endif
