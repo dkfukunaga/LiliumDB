dbb6f77b401a2365a8d2eb9b7ca312d2ed09bd58
dkfukunaga <dkfukunaga@gmail.com>
2026-03-05 00:56:38 -0800

    moved PageType to file_format.h

--- src/common/types.h
+++ src/common/types.h
@@ -11,15 +11,6 @@ using SlotNum = uint16_t;
 using PageOffset = uint16_t;
 using RecordSize = uint16_t;
 
-enum class PageType : uint8_t {
-    Invalid     = 0,    // invalid/unitialized
-    Table       = 1,    // table page
-    Index       = 2,    // index page
-    Expansion   = 3,    // overflow (future)
-    FreeList    = 4,    // free list
-    FreeSpace   = 5,    // Freespace map (future)
-};
-
 // sentinel values for invalid offsets
 inline constexpr PageNum INVALID_PAGE = std::numeric_limits<PageNum>::max();
 inline constexpr SlotNum INVALID_SLOT = std::numeric_limits<SlotNum>::max();
