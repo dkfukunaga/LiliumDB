57803626bcc8232940637f19f5b44ebfdf26424a
dkfukunaga <dkfukunaga@gmail.com>
2026-03-05 15:53:54 -0800

    Solidified Flags API and operatror overloads. Added toBitString() to be able to easily inspect bit values for debugging.

--- src/common/flags.h
+++ src/common/flags.h
@@ -3,6 +3,7 @@
 
 #include <type_traits>
 #include <initializer_list>
+#include <string>
 
 namespace LiliumDB {
 
@@ -14,108 +15,125 @@ public:
     using EnumType = Enum;
     using Int = std::underlying_type_t<Enum>;
 
-    Flags() : flags_(0) {}
-    Flags(Enum flag) : flags_(static_cast<Int>(flag)) {}
-    Flags(std::initializer_list<Enum> flags) : flags_(0) {
+    Flags() noexcept : flags_(0) {}
+    Flags(Enum flag) noexcept : flags_(toInt(flag)) {}
+    Flags(std::initializer_list<Enum> flags) noexcept : flags_(0) {
         for (Enum f : flags) {
-            flags_ |= static_cast<Int>(f);
+            flags_ |= toInt(f);
         }
     }
 
-    Flags& set(Enum flag, bool on = true) {
+    Flags& set(Enum flag, bool on = true) noexcept {
         if (on) {
-            flags_ |= static_cast<Int>(flag);
+            flags_ |= toInt(flag);
         } else {
-            flags_ &= ~static_cast<Int>(flag);
+            flags_ &= ~toInt(flag);
         }
         return *this;
     }
-    Flags& setAll(std::initializer_list<Enum> flags, bool on = true) {
+    Flags& setAll(std::initializer_list<Enum> flags, bool on = true) noexcept {
         for (Enum f : flags) {
             set(f, on);
         }
         return *this;
     }
-
-    bool has(Enum flag) const {
-        return (flags_ & static_cast<Int>(flag)) != 0;
+    Flags& setAll(Flags flags, bool on = true) noexcept {
+        if (on) {
+            flags_ |= flags.flags_;
+        } else {
+            flags_ &= ~flags.flags_;
+        }
+        return *this;
     }
-    bool hasAny(std::initializer_list<Enum> flags) const {
+    Flags& toggle(Enum flag) noexcept {
+        flags_ ^= toInt(flag);
+        return *this;
+    }
+    Flags& remove(Enum flag) noexcept {
+        set(flag, false);
+        return *this;
+    }
+    Flags& clear() noexcept {
+        flags_ = 0;
+        return *this;
+    }
+    
+
+    bool has(Enum flag) const noexcept {
+        return (flags_ & toInt(flag)) != 0;
+    }
+    bool hasAny(std::initializer_list<Enum> flags) const noexcept {
         for (Enum f : flags) {
-            if ((flags_ & static_cast<Int>(f)) != 0) {
+            if ((flags_ & toInt(f)) != 0) {
                 return true;
             }
         }
         return false;
     }
-    bool hasAny(Flags flags) const {
+    bool hasAny(Flags flags) const noexcept {
         return (flags_ & flags.flags_) != 0;
     }
-    bool hasAll(std::initializer_list<Enum> flags) const {
+    bool hasAll(std::initializer_list<Enum> flags) const noexcept {
         for (Enum f : flags) {
-            if ((flags_ & static_cast<Int>(f)) == 0) {
+            if ((flags_ & toInt(f)) == 0) {
                 return false;
             }
         }
         return true;
     }
-    bool hasAll(Flags flags) const {
+    bool hasAll(Flags flags) const noexcept {
         return (flags_ & flags.flags_) == flags.flags_;
     }
-    
 
-    Flags operator|(Enum flag) const {
-        return Flags(flags_ | static_cast<Int>(flag));
-    }
-    Flags operator|(Flags flag) const {
-        return Flags(flags_ | flag.flags_);
-    }
-    Flags operator&(Enum flag) const {
-        return Flags(flags_ & static_cast<Int>(flag));
-    }
-    Flags operator&(Flags flag) const {
-        return Flags(flags_ & flag.flags_);
-    }
-    Flags operator~() const {
-        return Flags(~flags_);
-    }
-    bool operator==(Flags other) const {
-        return flags_ == other.flags_;
-    }
-    bool operator!=(Flags other) const {
-        return flags_ != other.flags_;
-    }
-    bool operator!() const {
-        return flags_ == 0;
+    std::string toBitString() const {
+        using UInt = std::make_unsigned_t<Int>;
+        constexpr int width = int(sizeof(Int) * 8);
+
+        std::string str;
+        str.reserve(width);
+
+        UInt u = static_cast<UInt>(flags_);
+        for (int i = width - 1; i >= 0; --i) {
+            str += (u & (UInt(1) << i)) ? '1' : '0';
+            if (i % 4 == 0 && i != 0) str += ' ';
+        }
+        return str;
     }
 
-    Flags& operator|=(Enum flag) {
-        flags_ |= static_cast<Int>(flag);
-        return *this;
-    }
-    Flags& operator|=(Flags flag) {
-        flags_ |= flag.flags_;
-        return *this;
-    }
-    Flags& operator&=(Enum flag) {
-        flags_ &= static_cast<Int>(flag);
-        return *this;
-    }
-    Flags& operator&=(Flags flag) {
-        flags_ &= flag.flags_;
-        return *this;
-    }
+    Int bits() const noexcept { return flags_; }
+    explicit operator Int() const noexcept { return flags_; }
+    explicit operator bool() const noexcept { return flags_ != 0; }
 
-    explicit operator bool() const {
-        return flags_ != 0;
-    }
-    operator Int() const {
-        return flags_;
-    }
+    Flags operator~() const noexcept { return Flags(~flags_); }
+    Flags& operator|=(Enum other) noexcept { flags_ |= toInt(other); return *this; }
+    Flags& operator|=(Flags other) noexcept { flags_ |= other.flags_; return *this; }
+    Flags& operator&=(Enum other) noexcept { flags_ &= toInt(other); return *this; }
+    Flags& operator&=(Flags other) noexcept { flags_ &= other.flags_; return *this; }
+    Flags& operator^=(Enum other) noexcept { flags_ ^= toInt(other); return *this; }
+    Flags& operator^=(Flags other) noexcept { flags_ ^= other.flags_; return *this; }
+
+    friend bool operator==(Flags lhs, Flags rhs) noexcept { return lhs.flags_ == rhs.flags_; }
+    friend bool operator!=(Flags lhs, Flags rhs) noexcept { return lhs.flags_ != rhs.flags_; }
+
+    friend Flags operator|(Flags lhs, Flags rhs) noexcept { lhs |= rhs; return lhs; }
+    friend Flags operator|(Flags lhs, Enum rhs) noexcept { lhs |= rhs; return lhs; }
+    friend Flags operator|(Enum lhs, Flags rhs) noexcept { return rhs | lhs; }
+
+    friend Flags operator&(Flags lhs, Flags rhs) noexcept { lhs &= rhs; return lhs; }
+    friend Flags operator&(Flags lhs, Enum rhs) noexcept { lhs &= rhs; return lhs; }
+    friend Flags operator&(Enum lhs, Flags rhs) noexcept { return rhs & lhs; }
+
+    friend Flags operator^(Flags lhs, Flags rhs) noexcept { lhs ^= rhs; return lhs; }
+    friend Flags operator^(Flags lhs, Enum rhs) noexcept { lhs ^= rhs; return lhs; }
+    friend Flags operator^(Enum lhs, Flags rhs) noexcept { return rhs ^ lhs; }
+
 
 private:
-    explicit Flags(Int flags) : flags_(flags) {}
+    explicit Flags(Int flags) noexcept : flags_(flags) {}
+
     Int flags_;
+
+    static constexpr Int toInt(Enum flag) noexcept { return static_cast<Int>(flag); }
 };
 
 } // namespace LiliumDB
