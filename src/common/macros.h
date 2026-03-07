baecbd2939811802f41805423354cb198ecb7e40
dkfukunaga <dkfukunaga@gmail.com>
2026-03-07 10:03:00 -0800

    revised documentation.

--- src/common/macros.h
+++ src/common/macros.h
@@ -16,30 +16,30 @@ namespace LiliumDB {
         if (_s.isErr()) return _s.error();  \
     } while(0)
 
-// Evaluates expr (which must return a Status) and propagates the Status
-// error to the caller on failure.
+// Evaluates expr (which must return a Status) and wraps the Status error
+// in Result<T> before propagating it to the caller on failure.
 #define TRY_RESULT_WRAP(expr)               \
     do {                                    \
         auto _s = (expr);                   \
         if (_s.isErr()) return Err(_s);     \
     } while(0)
 
-// Evaluates expr (which must return a Result<T>), and either binds the
-// unwrapped value to var or propagates the Status error to the caller.
 #define TRY_STATUS_BIND_IMPL(expr, var, N)      \
     auto _r_##N = (expr);                       \
     if (_r_##N.isErr()) return _r_##N.error();  \
     auto var = std::move(_r_##N.value())
 
+// Evaluates expr (which must return a Result<T>), and either binds the
+// unwrapped value to var or propagates the Status error to the caller.
 #define TRY_STATUS_BIND(expr, var) TRY_STATUS_BIND_IMPL(expr, var, __COUNTER__)
 
-// Evaluates expr (which must return a Result<T>), and either binds the
-// unwrapped value to var or propagates the wrapped error to the caller.
 #define TRY_RESULT_BIND_IMPL(expr, var, N)              \
     auto _r_##N = (expr);                               \
     if (_r_##N.isErr()) return Err(_r_##N.error());     \
     auto var = std::move(_r_##N.value())
 
+// Evaluates expr (which must return a Result<T>), and either binds the
+// unwrapped value to var or propagates the wrapped error to the caller.
 #define TRY_RESULT_BIND(expr, var) TRY_RESULT_BIND_IMPL(expr, var, __COUNTER__)
 
 } // namespace LiliumDB
