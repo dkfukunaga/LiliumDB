990fa8700f0001a85eab0b423965fb1fbdc9ae8c
dkfukunaga <dkfukunaga@gmail.com>
2026-03-07 00:14:15 -0800

    Removed redundant macro, added macro to wrap Status in Err()

--- src/common/macros.h
+++ src/common/macros.h
@@ -8,31 +8,21 @@ namespace LiliumDB {
 
 // --- Error propagation macros
 
-// Evaluates expr (which must return a Status) and propagates the Status
-// error to the caller on failure.
+// Evaluates expr (which must return a Result<T> or a Status) and
+// propagates the Status error to the caller on failure.
 #define TRY_STATUS(expr)                    \
     do {                                    \
         auto _s = (expr);                   \
         if (_s.isErr()) return _s.error();  \
     } while(0)
 
-// Evaluates expr (which must return a Result<T>), and either forwards
-// the wrapped value or propagates the wrapped error to the caller.
-#define TRY_RESULT_FWD(expr)                    \
-    do {                                        \
-        auto _r = (expr);                       \
-        if (_r.isErr()) return Err(_r.error()); \
-        return Ok(_r.value());                  \
-    } while (0)
-
-// Evaluates expr (which must return a Result<T>), and either binds the
-// unwrapped value to var or propagates the wrapped error to the caller.
-#define TRY_RESULT_BIND_IMPL(expr, var, N)          \
-    auto _r_##N = (expr);                           \
-    if (_r_##N.isErr()) return Err(_r_##N.error()); \
-    auto var = std::move(_r_##N.value())
-
-#define TRY_RESULT_BIND(expr, var) TRY_RESULT_BIND_IMPL(expr, var, __COUNTER__)
+// Evaluates expr (which must return a Status) and propagates the Status
+// error to the caller on failure.
+#define TRY_RESULT_WRAP(expr)               \
+    do {                                    \
+        auto _s = (expr);                   \
+        if (_s.isErr()) return Err(_s);     \
+    } while(0)
 
 // Evaluates expr (which must return a Result<T>), and either binds the
 // unwrapped value to var or propagates the Status error to the caller.
@@ -42,6 +32,15 @@ namespace LiliumDB {
     auto var = std::move(_r_##N.value())
 
 #define TRY_STATUS_BIND(expr, var) TRY_STATUS_BIND_IMPL(expr, var, __COUNTER__)
+
+// Evaluates expr (which must return a Result<T>), and either binds the
+// unwrapped value to var or propagates the wrapped error to the caller.
+#define TRY_RESULT_BIND_IMPL(expr, var, N)              \
+    auto _r_##N = (expr);                               \
+    if (_r_##N.isErr()) return Err(_r_##N.error());     \
+    auto var = std::move(_r_##N.value())
+
+#define TRY_RESULT_BIND(expr, var) TRY_RESULT_BIND_IMPL(expr, var, __COUNTER__)
 
 } // namespace LiliumDB
 
