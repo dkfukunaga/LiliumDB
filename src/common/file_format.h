c4645d31f2b7bdb6ca2a98097966c5aa0c33f532
dkfukunaga <dkfukunaga@gmail.com>
2026-03-08 21:58:48 -0700

    fixed checksum placeholder value to write correctly in little endian

--- src/common/file_format.h
+++ src/common/file_format.h
@@ -17,7 +17,7 @@ inline constexpr uint8_t VERSION_MAJOR = 0;
 inline constexpr uint8_t VERSION_MINOR = 0;
 
 // checksum placeholder value
-inline constexpr uint32_t CHECKSUM_PLACEHOLDER = 0xDEADBEEF;
+inline constexpr uint32_t CHECKSUM_PLACEHOLDER = 0xEFBEADDE;
 
 // page size
 inline constexpr uint16_t PAGE_SIZE = 4096;
