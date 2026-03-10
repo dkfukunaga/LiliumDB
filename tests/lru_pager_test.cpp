edacca6bf760f8b5d2890fa46ab17dece4d15036
dkfukunaga <dkfukunaga@gmail.com>
2026-03-09 17:42:55 -0700

    Added test for LRUPager. File is not automatically deleted to allow for manual inspection.

--- tests/lru_pager_test.cpp
+++ tests/lru_pager_test.cpp
@@ -14,3 +14,35 @@ TEST(LRUPagerTest, OpenClose) {
     }
     // pager goes out of scope and should close without error
 }
+
+TEST(LRUPagerTest, NewPage) {
+    std::string path = "test_db_file.db";
+    {
+        auto result = LRUPager::open(path, OpenMode::ReadWrite);
+        ASSERT_TRUE(result.isOk());
+        auto pager = std::move(result.value());
+        ASSERT_TRUE(pager->isOpen());
+
+        // append a Table page
+        {
+            auto result = pager->newPage(PageType::Table);
+            ASSERT_TRUE(result.isOk());
+            auto page = std::move(result.value());
+        }
+
+        // append an Index page
+        {
+            auto result = pager->newPage(PageType::Index);
+            ASSERT_TRUE(result.isOk());
+            auto page = std::move(result.value());
+        }
+
+        // append a FreeList page
+        {
+            auto result = pager->newPage(PageType::FreeList);
+            ASSERT_TRUE(result.isOk());
+            auto page = std::move(result.value());
+        }
+    }
+    // pager goes out of scope and should close without error
+}
