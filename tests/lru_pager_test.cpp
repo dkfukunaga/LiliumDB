22abd63862228a70fc61bf69b34682c5f5cbfe57
dkfukunaga <dkfukunaga@gmail.com>
2026-03-09 21:18:07 -0700

    Test now deletes test db file if it already exists before running tests, which still leaves it to be inspected manually afterwards.

--- tests/lru_pager_test.cpp
+++ tests/lru_pager_test.cpp
@@ -1,26 +1,42 @@
 #include "pager/lru_pager.h"
 
+#include <filesystem>
+
 #include "gtest/gtest.h"
 
 using namespace LiliumDB;
 
-TEST(LRUPagerTest, OpenClose) {
+class LRUPagerTest : public ::testing::Test {
+protected:
     std::string path = "test_db_file.db";
+    std::unique_ptr<Pager> pager = nullptr;
+
+    void SetUp() override {
+        if (std::filesystem::exists((path))) {
+            std::filesystem::remove(path);
+        }
+    }
+
+    void TearDown() override {
+        
+    }
+};
+
+TEST_F(LRUPagerTest, OpenClose) {
     {
         auto result = LRUPager::open(path, OpenMode::ReadWrite);
         ASSERT_TRUE(result.isOk());
-        auto pager = std::move(result.value());
+        pager = std::move(result.value());
         ASSERT_TRUE(pager->isOpen());
     }
     // pager goes out of scope and should close without error
 }
 
-TEST(LRUPagerTest, NewPage) {
-    std::string path = "test_db_file.db";
+TEST_F(LRUPagerTest, NewPage) {
     {
         auto result = LRUPager::open(path, OpenMode::ReadWrite);
         ASSERT_TRUE(result.isOk());
-        auto pager = std::move(result.value());
+        pager = std::move(result.value());
         ASSERT_TRUE(pager->isOpen());
 
         // append a Table page
@@ -37,9 +53,9 @@ TEST(LRUPagerTest, NewPage) {
             auto page = std::move(result.value());
         }
 
-        // append a FreeList page
-        {
-            auto result = pager->newPage(PageType::FreeList);
+        // append 5 Table pages
+        for (int i = 0; i < 5; ++i) {
+            auto result = pager->newPage(PageType::Table);
             ASSERT_TRUE(result.isOk());
             auto page = std::move(result.value());
         }
