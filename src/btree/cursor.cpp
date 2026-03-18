614c082509682b70c1d8939bdcb55e376fc12262
dkfukunaga <dkfukunaga@gmail.com>
2026-03-18 01:37:23 -0700

    implemented Cursor

---
+++ src/btree/cursor.cpp
@@ -0,0 +1,74 @@
+#include "cursor.h"
+
+namespace LiliumDB {
+
+Cursor::Cursor(Cursor&& other) noexcept
+    : pager_(other.pager_)
+    , page_(std::move(other.page_))
+    , slot_(other.slot_) {
+    other.invalidate();
+}
+
+ByteView Cursor::key() const {
+    assert(valid_);
+    if (!valid_) {
+        return ByteView();
+    }
+    return getKey(page_, slot_);
+}
+
+ByteView Cursor::value() const {
+    assert(valid_);
+    if (!valid_) {
+        return ByteView();
+    }
+    return getValue(page_, slot_);
+}
+
+DbResult<void> Cursor::next() {
+    PageHeader header = page_.getHeader();
+
+    if (header.slotCount > 0 && slot_ < header.slotCount - 1) {
+        slot_++;
+    } else {
+        if (header.next == INVALID_PAGE) {
+            invalidate();
+        } else {
+            ASSIGN_OR_RETURN(page_, pager_.fetchPage(header.next));
+            header = page_.getHeader();
+
+            if (header.slotCount == 0) {
+                invalidate();
+            } else {
+                slot_ = 0;
+            }
+        }
+    }
+
+    return Ok();
+}
+
+DbResult<void> Cursor::prev() {
+    PageHeader header = page_.getHeader();
+
+    if (slot_ > 0) {
+        slot_--;
+    } else {
+        if (header.prev == INVALID_PAGE) {
+            invalidate();
+        } else {
+            ASSIGN_OR_RETURN(page_, pager_.fetchPage(header.prev));
+            header = page_.getHeader();
+
+            if (header.slotCount == 0) {
+                invalidate();
+            } else {
+                slot_ = header.slotCount - 1;
+            }
+        }
+    }
+
+    return Ok();
+}
+
+} // namespace LiliumDB
--- src/btree/cursor.h
+++ src/btree/cursor.h
@@ -2,27 +2,40 @@
 #define LILIUMDB_CURSOR_H
 
 #include "common/core.h"
+#include "pager/pager.h"
 #include "pager/page_guard.h"
 #include "utils/byte_span.h"
+#include "btree_page.h"
 
 namespace LiliumDB {
 
 class Cursor {
 public:
-    Cursor();
-    ~Cursor();
+    Cursor(Pager& pager, PageGuard page, SlotIndex slot)
+        : pager_(pager)
+        , page_(std::move(page))
+        , slot_(slot) { }
+    Cursor(const Cursor&) = delete;
+    Cursor(Cursor&& other) noexcept;
+    ~Cursor() = default;
 
-    ByteSpan        key() const;
-    ByteSpan        value() const;
+    ByteView        key() const;
+    ByteView        value() const;
     bool            valid() const { return valid_; }
 
     DbResult<void>  next();
     DbResult<void>  prev();
+
 private:
-    Pager*      pager_;
+    Pager&      pager_;
     PageGuard   page_;
     SlotIndex   slot_;
     bool        valid_;
+
+    void        invalidate() {
+        slot_ = INVALID_SLOT;
+        valid_ = false;
+    }
 };
 
 } // namespace LiliumDB
